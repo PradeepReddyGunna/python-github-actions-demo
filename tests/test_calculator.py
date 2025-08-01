035a67caf5164b308cd6bdf75c5b206af87e0d1c
PradeepReddyGunna <gunnapradeepreddy07@gmail.com>
2025-08-01 16:53:36 -0500

    Initial commit: Python app with GitHub Actions

---
+++ tests/test_calculator.py
@@ -0,0 +1,15 @@
+# tests/test_calculator.py
+
+import unittest
+from app.calculator import add, subtract
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
+
