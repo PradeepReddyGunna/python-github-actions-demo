01a488749b2c66810077d7c1075f5f7f098b0864
PradeepReddyGunna <gunnapradeepreddy07@gmail.com>
2025-08-01 17:02:55 -0500

    change in values

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,10 +5,10 @@ from app.calculator import add, subtract
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 6)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 2), 3)
+        self.assertEqual(subtract(5, 2), 8)
 
 if __name__ == '__main__':
     unittest.main()
